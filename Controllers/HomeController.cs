77cb563cc3ffdae630934ef1c8369ef7de9b2c51
Zepik <m.patek002@gmail.com>
2017-05-20 21:53:40 +0200

    Dodano możliwośc przesyłania plików

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using CreateIssueJira.Model;
 using CreateIssueJira.Services;
 using CreateIssueJira.ViewModels;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -15,7 +17,6 @@ namespace CreateIssueJira.Controllers
 
         private JiraIssueService _issueService;
         private IConfigurationRoot _Configuration;
-
         public HomeController(JiraIssueService issueService, IConfigurationRoot Configuration)
          {
              _issueService = issueService;
@@ -29,14 +30,18 @@ namespace CreateIssueJira.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Index(string Summary, string Description, string Reporter)
+        public async Task<IActionResult> Index(string Summary, string Description, string Reporter, IList<IFormFile> files)
         {
             JSON_issue issueToCreate = new JSON_issue(_Configuration["Issue:projectId"], Summary, Description, _Configuration["Issue:issueType"], Reporter);
             IssueCreateResponse respone = await _issueService.CreateIssueJira(issueToCreate);
-            
+            List<string> filesnames;
+            if(files.Count > 0)
+            {
+                filesnames = _issueService.UploadAttachments(files);  
+                var respone2 = await _issueService.SendAttachmentsToIssueJira(filesnames, respone.key);
+            }
             return View("IssueCreated", respone);
         }
-
         public IActionResult Error()
         {
             var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
--- Services/JiraIssueService.cs
+++ Services/JiraIssueService.cs
@@ -8,50 +8,98 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using CreateIssueJira.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace CreateIssueJira.Services
 {
 
-    public class JiraIssueService
-    {
-        private IConfigurationRoot _Configuration;
-        private readonly ILogger _logger;
-        public JiraIssueService(IConfigurationRoot Configuration,  ILogger<JiraIssueService> logger)
-        {
-            _Configuration = Configuration;
-            _logger = logger;
-        }
+	public class JiraIssueService
+	{
+		private IConfigurationRoot _Configuration;
+		private readonly ILogger _logger;
+		private IHostingEnvironment _env;
+		public JiraIssueService(IConfigurationRoot Configuration,  ILogger<JiraIssueService> logger, IHostingEnvironment env)
+		{
+			_Configuration = Configuration;
+			_logger = logger;
+			_env = env;
+		}
 
-        public async Task<IssueCreateResponse> CreateIssueJira(JSON_issue issue)
-        {
+		public async Task<IssueCreateResponse> CreateIssueJira(JSON_issue issue)
+		{   
 
-                var client = new HttpClient();
-                var Credentials = Encoding.ASCII.GetBytes(_Configuration["Credentials:Jira"]);
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Credentials));
-                var jsonInString = JsonConvert.SerializeObject(issue);
-                var stringcontent = new StringContent(jsonInString, Encoding.UTF8, "application/json");
-                var response = await client.PostAsync(_Configuration["Url:Base"] + "/jira/rest/api/2/issue", stringcontent);
-                var response_string_json = response.Content.ReadAsStringAsync().Result;
-                var respone_json = JsonConvert.DeserializeObject<IssueCreateResponse>(response_string_json); 
+			IssueCreateResponse respone_json;
+			using(var client = new HttpClient())
+			{
+				var Credentials = Encoding.ASCII.GetBytes(_Configuration["Credentials:Jira"]);
+				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Credentials));
+				var jsonInString = JsonConvert.SerializeObject(issue);
+				var stringcontent = new StringContent(jsonInString, Encoding.UTF8, "application/json");
+				var response = await client.PostAsync(_Configuration["Url:Base"] + "/jira/rest/api/2/issue", stringcontent);
+				var response_string_json = response.Content.ReadAsStringAsync().Result;
+				respone_json = JsonConvert.DeserializeObject<IssueCreateResponse>(response_string_json); 
+			}
+			if(respone_json.key != null)
+			{
+				_logger.LogInformation($"Utworzono zgłoszenie: {respone_json.key}");
+			}
+			else
+			{
+				if(respone_json.errors != null)
+				{
+					_logger.LogError($" {respone_json.errors}");
+				}
+				foreach(string error in respone_json.errorMessages)
+				{
+					 _logger.LogError($" {error}");
+				}
+			}               
+			return respone_json; 
 
-            if(respone_json.key != null)
-            {
-                _logger.LogInformation($"Utworzono zgłoszenie: {respone_json.key}");
-            }
-            else
-            {
-                if(respone_json.errors != null)
-                {
-                    _logger.LogError($" {respone_json.errors}");
-                }
-                foreach(string error in respone_json.errorMessages)
-                {
-                     _logger.LogError($" {error}");
-                }
-            }
-            return respone_json; 
+		}
 
-        }
+		public List<string> UploadAttachments(IList<IFormFile> files)
+		{
+			List<string> filesnames = new List<string>();
+			foreach(var file in files)
+			{
+				filesnames.Add(file.FileName);
+				var filename = ContentDispositionHeaderValue
+								.Parse(file.ContentDisposition)
+								.FileName
+								.Trim('"');
+				filename = _env.WebRootPath + $@"/zalaczniki/{filename}";
+				using (FileStream fs = System.IO.File.Create(filename))
+				{
+				file.CopyTo(fs);
+				fs.Flush();
+				}
+			}
+            return filesnames;
+		}
 
-    }
+		public async Task<List<HttpResponseMessage>> SendAttachmentsToIssueJira(List<string> filesNames, string issueKey)
+		{   
+			List<HttpResponseMessage> response = new List<HttpResponseMessage>();
+			using(var client = new HttpClient())
+			{
+				var Credentials = Encoding.ASCII.GetBytes(_Configuration["Credentials:Jira"]);
+				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Credentials));
+				client.DefaultRequestHeaders.Add("X-Atlassian-Token", "nocheck");
+				foreach(var file in filesNames)
+				{
+					MultipartFormDataContent content = new MultipartFormDataContent();
+					HttpContent fileContent = new ByteArrayContent(File.ReadAllBytes(_env.WebRootPath + $@"/zalaczniki/{file}"));
+					content.Add(fileContent, "file", file);
+					string url = $"http://patek-ms-7758:2990/jira/rest/api/2/issue/{issueKey}/attachments";
+					response.Add(await client.PostAsync(url, content));
+				}
+				
+			}
+			return response; 
+		}
+}
 }
